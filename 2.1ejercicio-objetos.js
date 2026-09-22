cce569bd4efa7bad03960b8055b3cd9bbbd61dea
ESKELETO_MALO <camilo.soto.alp@cbtis258.edu.mx>
2026-09-21 19:53:07 -0600

    Add files via upload

---
+++ 2.1ejercicio-objetos.js
@@ -0,0 +1,13 @@
+// Actividad 5 ejercicios basicos de objetos JS 18-09-26
+// Crear un objeto para representar una entidad del mundo real
+// Despues imprime el objeto en console agrega tu nombre
+
+let Telefono = {
+    "Marca":"Xiaomi",
+    "Tamaño": 6,
+    "Color": "rojo",
+    "actualizaciones": true
+}
+
+console.log(typeof(Telefono))
+console.table(Telefono)
